2eebf9190c21ec31001fe218e294ea87b7ca0fad
worzell <jakobfred1999@gmail.com>
2021-12-14 17:37:21 +1300

    Added more events and new token path

--- DiscordBot/YaykobBot.py
+++ DiscordBot/YaykobBot.py
@@ -1,17 +1,25 @@
 import os
 import discord
+from dotenv import load_dotenv
+load_dotenv(dotenv_path = os.path.abspath(os.getcwd()) + "\DiscordBot\discordbot.env")
+TOKEN = os.getenv("CLIENT_TOKEN")
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"{client.user} logged in now!")
+    print(f"{client.user} is live!")
+    #channel = client.get_channel(328796156240986114)
+    #await channel.send(":flushed:")
 
 @client.event
 async def on_message(message):
-    print(message.content)
-    print(dir(message))
-    await message.delete()
+    if message.content.startswith("$yaykob"):
+        await message.channel.send(f"Hello {message.author}, you sent your message from {message.channel.name}!")
 
-my_secret = "OTIwMDYxNTEyMTg4NTc5ODYx.Ybe4Dg.S8KoYuj-GUjkthWZt9gxiL3YwmQ"
-client.run(my_secret)
+@client.event
+async def on_disconnect():
+    channel = client.get_channel(328796156240986114)
+    await channel.send("I'm shutting down peeps, peace out.")
+
+client.run(TOKEN)
